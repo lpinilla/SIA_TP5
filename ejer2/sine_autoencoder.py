0791db8d145ddf65cc601d0770a08fc2c77aeee8
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-11-15 15:22:06 -0300

    ejer 2 parecería funcionar

---
+++ ejer2/sine_autoencoder.py
@@ -0,0 +1,45 @@
+from MultilayerPerceptron import MultilayerPerceptron
+import pickle
+
+#input
+samples = pickle.load(open('sin_samples.pickle', 'rb'))
+
+sc = StandardScaler()
+stand_samples = sc.fit_transform(samples)
+
+_input    = samples
+_expected = samples
+
+nn = MultilayerPerceptron(optimizer='L-BFGS-B',eta=0.1, momentum=0.8, act_fun="tanh", split_data=False, test_p=0.1, use_momentum=True, adaptative_eta=False)
+
+#arq = [100, 50, 25, 5, 25, 50, 100]
+
+#arq = [100, 25, 5, 25, 100]
+
+#arq = [50, 25, 5, 2, 5, 25, 50]
+
+arq = [50, 25, 5, 25, 50]
+
+#arq = [50, 15, 10, 5, 2, 5, 10, 15, 50]
+
+#arq = [50, 25, 50]
+
+#nn.create_arq(arq)
+
+nn.entry_layer(50)
+nn.add_hidden_layer(25, beta=0.9)
+nn.add_hidden_layer(5, beta=0.5)
+#nn.add_hidden_layer(2, beta=0.5)
+#nn.add_hidden_layer(5, beta=0.6)
+nn.add_hidden_layer(25, beta=0.9)
+nn.output_layer(50)
+
+error = nn.train(_input, _expected, epochs=1500)
+
+if error < 4:
+    f = open('trained_weights.pickle', 'wb')
+    pickle.dump(nn.flatten_weights(), f)
+#print(samples[0])
+#print(nn.predict(samples[0]))
+#error = nn.train_minimizer(_input, _expected)
+
---
+++ ejer2/test_data/generate_sine.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pickle
+from MultilayerPerceptron import MultilayerPerceptron
+
+samples = pickle.load(open('sin_samples.pickle', 'rb'))
+
+trained_weights = pickle.load(open('trained_weights.pickle', 'rb'))
+
+nn = MultilayerPerceptron(optimizer='L-BFGS-B',eta=0.1, momentum=0.8, act_fun="tanh", split_data=False, test_p=0.1, use_momentum=True, adaptative_eta=False)
+
+#re-armar la arquitectura entrenada
+nn.entry_layer(50)
+nn.add_hidden_layer(25, beta=0.9)
+nn.add_hidden_layer(5, beta=0.5)
+nn.add_hidden_layer(25, beta=0.9)
+nn.output_layer(50)
+
+#cargar los pesos
+nn.rebuild_net(trained_weights)
+
+#tomar muestra del código latente de algún dato
+#coding_0 = nn.view_latent_coding(samples[0])
+#print(coding_0)
+#codificación de sample 0
+#[ 0.99264605 -0.88249352  0.96457358  0.9994266  -0.92983281]
+
+
+#generar algo a partir de la capa latente
+latent_try = [1, -0.7, 0.95, 1, -0.9]
+
+generated = nn.generate(latent_try)
+
+#for i in generated:
+#    print(i)
+
