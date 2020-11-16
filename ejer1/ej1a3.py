78812a20c69973f11f7df4d9cde784e691324cf5
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-11-16 13:04:56 -0300

    ejer 1.a.3 en archivo aparte

--- ejer1/ej1a3.py
+++ ejer1/ej1a3.py
@@ -4,9 +4,12 @@ import random
 import math
 from MultilayerPerceptron import MultilayerPerceptron
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 #cargar el dataset
 letras = pickle.load(open('../resources/letras.pickle', 'rb'))
+letras_etiquetas = ["space", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", "´", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?"]
+
 #cargar los pesos ya entrenados
 trained_weights = pickle.load(open('trained_weights.pickle','rb'))
 
@@ -30,3 +33,24 @@ nn.output_layer(35)
 nn.rebuild_net(trained_weights)
 
 #red lista para usar
+plot_vals = []
+
+for i in letras:
+    plot_vals.append(nn.view_latent_coding(i))
+
+x_plot = [i[0] for i in plot_vals]
+y_plot = [i[1] for i in plot_vals]
+colors = cm.rainbow(np.linspace(0, 1, len(plot_vals)))
+
+#graficar los puntos
+fig, ax = plt.subplots()
+ax.scatter(x_plot, y_plot, color=colors)
+
+#agregar las etiquetas
+for i in range(len(letras_etiquetas)):
+    ax.annotate(letras_etiquetas[i], (x_plot[i], y_plot[i]))
+
+ax.grid(True)
+
+plt.show()
+
